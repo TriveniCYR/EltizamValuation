b53f18081b6065beaf9a8b59cd1fc207115b1d96
pwnraj5722 <pawan.k@neosoftmail.com>
2023-10-03 13:30:01 +0530

    web commit

---
+++ Eltizam.Utility/Enums/ModuleEnum.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel;
+
+namespace Eltizam.Utility.Enums
+{
+    public enum ModuleEnum
+    {
+        UserManagement = 1,
+        RoleManagement = 2,
+        Dashboard = 3,
+        MasterManagement = 4,
+        PIDF = 5,
+        PBF = 6,
+        IPD = 7,
+        Finance = 8,
+        Medical = 9,
+        APIListManagement = 10,
+        Formulation = 11,
+        ManagementHOD = 12,
+        CommercialManagement = 15,
+        APIManagement =13
+    }
+
+    public enum ModulePermissionEnum
+    {
+        [Description("User Management")]
+        UserManagement = 1,
+
+		[Description("Role Management")]
+        RoleManagement = 2,
+
+        Dashboard = 3,
+
+        [Description("Master Management")]
+        MasterManagement = 4,
+
+        PIDF = 5,
+        PBF = 6,
+        IPD = 7,
+        Finance = 8,
+        Medical = 9,
+
+        [Description("API List Management")]
+        APIListManagement = 13,
+
+        [Description("Audit logs")]
+        Auditlogs = 14,
+
+        Commercial = 15,
+        ManagementHOD = 16,
+        Project = 17,
+      [Description("Wish List")]
+
+		WishList = 18,
+
+		[Description("TblUser Management")]
+		TblUserManagement = 19,
+
+	}
+
+	public enum SubModulePermissionEnum
+    {
+        RnD = 1,
+        Clinical = 2,
+        Analytical = 3,
+        APIRnD = 4,
+        APIIPD = 12,
+        APICharter = 13,
+        APICharterSummary = 14,
+        RA = 15
+    }
+
+    public enum PermissionEnum
+    {
+        Any = 0,
+        View = 1,
+        Add = 2,
+        Edit = 3,
+        Delete = 4,
+        Approve = 5
+    }
+    public enum IPDPatenDetailsType
+    {
+        PatenDetailsForFormulation = 1,
+        PatientDetailsForAPI = 2,
+    }
+}
---
+++ Eltizam.Web/Controllers/UserController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+using Eltizam.Business.Models;
+using Eltizam.Resource;
+using Eltizam.Utility.Enums;
+using Eltizam.Utility.Models;
+using Eltizam.Utility.Utility;
+using Eltizam.Web.Helpers;
+
+namespace Eltizam.Web.Controllers
+{
+	public class UserController : BaseController
+	{
+		#region Properties
+
+		private readonly IConfiguration _cofiguration;
+		private readonly IStringLocalizer<Errors> _stringLocalizerError;
+		private readonly IStringLocalizer<Shared> _stringLocalizerShared;
+		private Microsoft.AspNetCore.Hosting.IHostingEnvironment _env;
+		private readonly IHelper _helper;
+
+		#endregion Properties
+
+		public UserController(IConfiguration configuration, Microsoft.AspNetCore.Hosting.IHostingEnvironment env,
+			   IStringLocalizer<Errors> stringLocalizerError, IStringLocalizer<Shared> stringLocalizerShared, IHelper helper)
+		{
+			_cofiguration = configuration;
+			_stringLocalizerError = stringLocalizerError;
+			_stringLocalizerShared = stringLocalizerShared;
+			_env = env;
+			_helper = helper;
+		}
+		
+        public IActionResult TblUser()
+        {
+            int rolId = _helper.GetLoggedInRoleId();
+            RolePermissionModel objPermssion = UtilityHelper.GetCntrActionAccess((int)ModulePermissionEnum.UserManagement, rolId);
+            ViewBag._objPermission = objPermssion;
+            return View();
+        }
+     
+        public IActionResult TblUserManage(int? Id)
+        {
+            TblUserModel masterUser;
+            if (Id == null || Id <= 0)
+            {
+                masterUser = new TblUserModel();
+                return View(masterUser);
+            }
+            else
+            {
+                HttpContext.Request.Cookies.TryGetValue(UserHelper.EltizamToken, out string token);
+                APIRepository objapi = new(_cofiguration);
+                HttpResponseMessage responseMessage = objapi.APICommunication(APIURLHelper.GetTblUserById + "/" + Id, HttpMethod.Get, token).Result;
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    string jsonResponse = responseMessage.Content.ReadAsStringAsync().Result;
+                    var data = JsonConvert.DeserializeObject<APIResponseEntity<TblUserModel>>(jsonResponse);
+                    if (data._object is null)
+                        return NotFound();
+
+                    return View(data._object);
+                }
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult TblUserManage(int Id, TblUserModel masterUser)
+        {
+            try
+            {
+                masterUser.Password = string.IsNullOrEmpty(masterUser.Password) ? "" : masterUser.Password;
+                masterUser.ConfirmPassowrd = string.IsNullOrEmpty(masterUser.ConfirmPassowrd) ? "" : masterUser.ConfirmPassowrd;
+                masterUser.LoggedUserId = _helper.GetLoggedInUserId();
+               
+            }
+            catch (Exception e)
+            {
+                _helper.LogExceptions(e);
+                TempData[UserHelper.ErrorMessage] = Convert.ToString(e.StackTrace);
+                return RedirectToAction("TblUserManage", new { UserId = masterUser.Id });
+            }
+            try
+            {
+                masterUser.WebApplicationUrl = HttpContext.Request.Scheme + "://" + HttpContext.Request.Host.Value;
+                HttpContext.Request.Cookies.TryGetValue(UserHelper.EltizamToken, out string token);
+                APIRepository objapi = new(_cofiguration);
+
+                HttpResponseMessage responseMessage = objapi.APICommunication(APIURLHelper.SaveTblUser, HttpMethod.Post, token, new StringContent(JsonConvert.SerializeObject(masterUser))).Result;
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    string jsonResponse = responseMessage.Content.ReadAsStringAsync().Result;
+                    TempData[UserHelper.SuccessMessage] = Convert.ToString(_stringLocalizerShared["RecordInsertUpdate"]);
+
+                    return RedirectToAction(nameof(TblUser));
+                }
+                else
+                {
+                    TempData[UserHelper.ErrorMessage] = Convert.ToString(responseMessage.Content.ReadAsStringAsync().Result);
+                    return RedirectToAction("TblUserManage", new { UserId = masterUser.Id });
+                }
+            }
+            catch (Exception e)
+            {
+                _helper.LogExceptions(e);
+                TempData[UserHelper.ErrorMessage] = Convert.ToString(e.StackTrace);
+                return RedirectToAction("TblUserManage", new { UserId = masterUser.Id });
+            }
+        }
+    }
+}
