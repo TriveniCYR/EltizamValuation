7d71fce2f38a5e9dca0b9b1038bd911551f46e62
YallaReddy <yalla.reddy@datatusk.com>
2023-10-13 16:05:22 +0530

    Location fixes

--- Eltizam.Business.Models/DataTableAjaxPostModel.cs
+++ Eltizam.Business.Models/DataTableAjaxPostModel.cs
@@ -11,7 +11,7 @@ namespace Eltizam.Business.Models
         // properties are not capital due to json mapping
         public int draw { get; set; }
         public int start { get; set; }
-        public int length { get; set; }
+        public int length { get; set; } = 10000;
         public int page { get; set; }
         public int pages { get; set; }
         public List<Column>? columns { get; set; }
