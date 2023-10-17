7ddfb3bd8103ad56f2979440c038df658cacf3f3
YallaReddy <yalla.reddy@datatusk.com>
2023-10-17 22:31:16 +0530

    Export to pbf, cvs fixes

--- Eltizam.Web/wwwroot/js/Custom/DynamicDataTable.js
+++ Eltizam.Web/wwwroot/js/Custom/DynamicDataTable.js
@@ -31,7 +31,17 @@
             },
             buttons: [
                 {
-                    extend: 'excel', text: '<i class="far fa-file-excel"></i> Export In Excel ', className: "btn-primary", exportOptions: {
+                    extend: 'excel', text: '<i class="far fa-file-excel"></i> Excel ', className: "btn-primary", exportOptions: {
+                        columns: ':not(.notexport)'
+                    }
+                },
+                {
+                    extend: 'pdf', text: '<i class="far fa-file-pdf"></i> PDF ', className: "btn-primary", exportOptions: {
+                        columns: ':not(.notexport)'
+                    }
+                },
+                {
+                    extend: 'csv', text: '<i class="far fa-file-csv"></i> CSV ', className: "btn-primary", exportOptions: {
                         columns: ':not(.notexport)'
                     }
                 },
