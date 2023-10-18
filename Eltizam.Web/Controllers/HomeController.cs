50e3f780280ae2404c6c1b0ba8ba0bf8bbd9d760
YallaReddy <yalla.reddy@datatusk.com>
2023-10-18 10:07:04 +0530

    Home page fixes

--- Eltizam.Web/Controllers/HomeController.cs
+++ Eltizam.Web/Controllers/HomeController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eltizam.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Eltizam.Web.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult AccessRestriction()
+        {
+            return View();
+        }
+        public IActionResult InternalServerError()
         {
             return View();
         }
