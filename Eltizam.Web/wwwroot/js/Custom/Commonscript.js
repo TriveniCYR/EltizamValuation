f14fad111e120afefb3bc1b0395b37d50c19801e
YallaReddy <yalla.reddy@datatusk.com>
2023-10-20 22:34:48 +0530

    Code review

--- Eltizam.Web/wwwroot/js/Custom/Commonscript.js
+++ Eltizam.Web/wwwroot/js/Custom/Commonscript.js
@@ -33,3 +33,13 @@ function GetActiveFlagCss(data) {
     }
     return dd;
 }
+
+
+function sideNavToggle() {
+    var x = document.getElementById("sideNav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
