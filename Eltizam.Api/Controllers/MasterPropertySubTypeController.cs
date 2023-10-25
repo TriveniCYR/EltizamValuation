860c6eb41e5fa82bf7e3bb12e20e8e6abcba0757
Abhishek <abhishek.nagar@neosoftmail.com>
2023-10-25 15:23:57 +0530

    commit sub property-1

--- Eltizam.Api/Controllers/MasterPropertySubTypeController.cs
+++ Eltizam.Api/Controllers/MasterPropertySubTypeController.cs
@@ -75,7 +75,7 @@ namespace EltizamValuation.Api.Controllers
             try
             {
                 var oPrpoertyTypeEntity = await _subpropertyServices.GetMasterSubPropertyByPropertyTypeIdAsync(id);
-                if (oPrpoertyTypeEntity != null && oPrpoertyTypeEntity.Id > 0)
+                if (oPrpoertyTypeEntity != null)
                     return _ObjectResponse.Create(oPrpoertyTypeEntity, (Int32)HttpStatusCode.OK);
                 else
                     return _ObjectResponse.Create(null, (Int32)HttpStatusCode.BadRequest, AppConstants.NoRecordFound);
