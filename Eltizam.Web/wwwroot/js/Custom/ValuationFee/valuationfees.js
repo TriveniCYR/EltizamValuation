424188342e458e69c2a83eb891f76edc6635693c
YallaReddy <yalla.reddy@datatusk.com>
2023-10-26 09:55:11 +0530

    Valuation fee fixes

--- Eltizam.Web/wwwroot/js/Custom/ValuationFee/valuationfees.js
+++ Eltizam.Web/wwwroot/js/Custom/ValuationFee/valuationfees.js
@@ -70,8 +70,8 @@ function InitializeDataList() {
             "data": "action", className: 'notexport actionColumn', "name": "Action", "render": function (data, type, row, meta) {
                 var html = '';
                 html += '<img src="../assets/dots-vertical.svg" alt="dots-vertical" class="activeDots" /> <div class="actionItem"><ul>'
-                html += '<li><a title="View" href="/MasterValuationFees/ValuationFeesDetail?id=' + row.id + '"><img src="../assets/view.svg" alt="view" />View</a></li>';
-                html += '<li><a title="Edit" href="/MasterValuationFees/ValuationFeesManage?id=' + row.id + '"><img src="../assets/edit.svg" alt="edit" />Edit</a></li>';
+                html += '<li><a title="View" href="/MasterValuationFee/ValuationFeeDetail?id=' + row.id + '"><img src="../assets/view.svg" alt="view" />View</a></li>';
+                html += '<li><a title="Edit" href="/MasterValuationFee/ValuationFeeManage?id=' + row.id + '"><img src="../assets/edit.svg" alt="edit" />Edit</a></li>';
                 html += '<li><a title="Delete" data-toggle="modal" data-target="#DeleteValuationFeesModel" data-backdrop="static" data-keyboard="false" onclick="ConfirmationDeleteValuationFees(' + row.id + ');"><img src="../assets/trash.svg" alt="trash" />Delete</a></li>';
                 html += '</ul></div>';
 
