3c2aaa85646f4dce04b09759198b280cfea8d4ae
YallaReddyNeo <mailto:yalla.reddy@neosofttech.com>
2023-10-30 16:15:55 +0530

    valution list api

---
+++ Eltizam.Business.Models/ValutionRequestListModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eltizam.Business.Models
+{
+    public class ValutionRequestListModel
+    {
+        public int Id { get; set; }
+       
+        public string? ClientName { get; set; }
+        public string? ClientType { get; set; }
+
+        public string? Location { get; set; }
+
+        public string? PropertyName { get; set; }
+
+        public DateTime? ValuationDate { get; set; }
+        public string? ValuationPurpose { get; set; }
+
+        
+    }
+}
