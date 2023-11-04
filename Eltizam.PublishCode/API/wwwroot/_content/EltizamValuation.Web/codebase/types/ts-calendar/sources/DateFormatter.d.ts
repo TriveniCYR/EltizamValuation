5c54dddc93218c8d85c3698fed0ee1d1f0f4ac3f
YallaReddy <yalla.reddy@datatusk.com>
2023-11-04 20:40:52 +0530

    delete publish

--- Eltizam.PublishCode/API/wwwroot/_content/EltizamValuation.Web/codebase/types/ts-calendar/sources/DateFormatter.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getFormatedDate(format: string, date: Date): string;
-export declare function stringToDate(str: string, format: string, validate?: boolean): any;
--- Eltizam.PublishCode/API/wwwroot/_content/EltizamValuation.Web/codebase/types/ts-grid/sources/ProGrid.d.ts
+++
@@ -1,8 +0,0 @@
-import { Grid } from "./Grid";
-import { IGridConfig } from "./types";
-export declare class ProGrid extends Grid {
-    constructor(container: HTMLElement | string, config?: IGridConfig);
-    protected _setEventHandlers(): void;
-    private _getColumnGhost;
-    private _dragStartColumn;
-}
