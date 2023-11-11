a52c3160005ae7a81a20e2b4a51ccea1e98d7b2f
YallaReddy <yalla.reddy@datatusk.com>
2023-11-11 17:02:51 +0530

    sd

--- EltizamValuationWebApi/obj/Debug/net6.0/Eltizam.WebApi.AssemblyInfo.cs
+++ EltizamValuationWebApi/obj/Debug/net6.0/Eltizam.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eltizam.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c64c8689dfb2df82356677376bbd68691a7b631a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc3da72e22adf9c43097c97127fceee5919b8171")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eltizam.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eltizam.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
